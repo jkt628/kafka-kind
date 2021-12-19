c7419b20f2a7ac5a6b72191a3a2c71b1b978b1cd
Jack Thomasson <thomasso@adobe.com>
2021-12-19 09:19:53 -0500

    render helm on git-bash
    Microsoft Windows is weird

--- render-local-registry.bat
+++ render-local-registry.bat
@@ -1,4 +1,5 @@
 @echo off
-more > render-local-registry.tmp
-bash render-local-registry < render-local-registry.tmp
-del render-local-registry.tmp
+more > render-local-registry.tmp0
+bash render-local-registry <render-local-registry.tmp0 >render-local-registry.tmp1 2>render-local-registry.tmp2
+more < render-local-registry.tmp1
+del render-local-registry.tmp?
